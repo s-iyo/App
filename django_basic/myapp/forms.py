d5fb294726c60d370b4601a5c9b13f4696a93448
shunsuke <s-iyo@unext-hd.jp>
2025-05-12 11:29:54 +0900

    諸変更

--- django_basic/myapp/forms.py
+++ django_basic/myapp/forms.py
@@ -4,7 +4,7 @@ from .models import Spot, Country
 class SpotForm(forms.ModelForm):
     class Meta:
         model = Spot
-        fields = ['country', 'name', 'information', 'best_season']  # photo を削除
+        fields = ['country', 'name', 'information', 'best_season']
         widgets = {
             'best_season': forms.CheckboxSelectMultiple,  # 複数選択をチェックボックスで表示
         }
