7d4636836ac2c3fbb27a0e0ed5fc7aa5bba2296b
shunsuke <s-iyo@unext-hd.jp>
2025-05-15 14:22:16 +0900

    タグ機能の追加

--- django_basic/myapp/forms.py
+++ django_basic/myapp/forms.py
@@ -4,9 +4,10 @@ from .models import Spot, Country
 class SpotForm(forms.ModelForm):
     class Meta:
         model = Spot
-        fields = ['country', 'name', 'information', 'best_season', 'photo']
+        fields = ['country', 'name', 'information', 'best_season', 'photo', 'tag']
         widgets = {
             'best_season': forms.CheckboxSelectMultiple,  # 複数選択をチェックボックスで表示
+            'tag': forms.CheckboxSelectMultiple,  # 複数選択をチェックボックスで表示
         }
 
 class CountryForm(forms.ModelForm):
--- django_basic/myapp/models.py
+++ django_basic/myapp/models.py
@@ -21,12 +21,19 @@ class Month(models.Model):
     def __str__(self):
         return self.name
 
+class Tags(models.Model):
+    name = models.CharField('タグ名', max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
 class Spot(models.Model):
     country = models.ForeignKey(Country, verbose_name='国', on_delete=models.CASCADE)
     name = models.CharField('観光地名', max_length=200)
     information = models.TextField('詳細情報')
     best_season = models.ManyToManyField(Month, verbose_name='ベストシーズン', blank=True)
     photo = models.ImageField(upload_to='myapp/picture/', blank=True, null=True, verbose_name="イメージ")
+    tag = models.ManyToManyField(Tags, verbose_name='タグ', blank=True)
 
     def __str__(self):
         return self.name
